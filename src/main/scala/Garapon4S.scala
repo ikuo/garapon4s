cc17cd7fffe093532d61f05a4899daafbf2dc63b
Ikuo Matsumura <makiczar@gmail.com>
2014-02-15 16:40:09 +0900

    Add stub of Garapon4S

---
+++ src/main/scala/Garapon4S.scala
@@ -0,0 +1,8 @@
+package com.github.ikuo.garapon4s
+
+object Garapon4S {
+  def main(args: Array[String]) {
+    println("Hello")
+  }
+}
+
---
+++ src/test/scala/Garapon4SSpec.scala
@@ -0,0 +1,12 @@
+package com.github.ikuo.garapon4s
+
+import org.scalatest.FunSpec
+
+class Garapon4SSpec extends FunSpec {
+  describe("Adding 1 to 1") {
+    it("should equals 2"){
+      assert(1+1 == 2)
+    }
+  }
+}
+
