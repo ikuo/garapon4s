55fdfdf5de270d0967b5d90d53b8a19d110ce7ed
Ikuo Matsumura <makiczar@gmail.com>
2014-03-09 16:49:29 +0900

    Add SearchResultSpec

--- src/main/scala/model/SearchResult.scala
+++ src/main/scala/model/SearchResult.scala
@@ -27,40 +27,47 @@ object SearchResult {
     val parser = jsonFactory.createParser(in)
     val listener = Option(resultListener).getOrElse(new BufferedListener)
 
-    try {
-      acceptToken(START_OBJECT, parser)
-      while(!parser.isClosed) {
-        parser.nextToken match {
-          case FIELD_NAME => parser.getText match {
-            case "status"   => listener.notifyStatus(intFieldValue(parser))
-            case "hit"      => listener.notifyHit(intFieldValue(parser))
-            case "version"  => listener.notifyVersion(parser.nextTextValue)
-            case "program"  => {
-              acceptToken(START_ARRAY, parser)
-              listener.notifyPrograms(parsePrograms(parser))
+    acceptToken(START_OBJECT, parser)
+    while(!parser.isClosed) {
+      parser.nextToken match {
+        case FIELD_NAME => parser.getText match {
+          case "status"   => listener.notifyStatus(intFieldValue(parser))
+          case "hit"      => listener.notifyHit(intFieldValue(parser))
+          case "version"  => listener.notifyVersion(parser.nextTextValue)
+          case "program"  => {
+            val iterator = new ProgramIterator(parser)
+            listener.notifyPrograms(iterator)
+            if (iterator.hasNext) {
+              throw new IllegalStateException(
+                "Program iterator is not completely consumed during the call to notifyPrograms()")
             }
-            case name => unexpectedField(name, parser)
           }
-          case END_OBJECT | null => ()
-          case token => unexpected(token, parser)
+          case name => unexpectedField(name, parser)
         }
+        case END_OBJECT | null => ()
+        case token => unexpected(token, parser)
       }
-    } catch {
-      case error: MalformedResponse => throw error
-      case error: RuntimeException =>
-        throw new MalformedResponse(error.getMessage, parser, error)
-      case error: Throwable => throw error
     }
 
     listener.getResult
   }
 
-  protected def parsePrograms(parser: JsonParser): Stream[Program] =
-    if (parser.nextToken == END_ARRAY || parser.isClosed) { Stream.empty }
-    else {
-      val program = objectMapper.readValue(parser, classOf[Program])
-      program #:: parsePrograms(parser)
+  class ProgramIterator(val parser: JsonParser) extends Iterator[Program] {
+    acceptToken(START_ARRAY, parser)
+    private var _next: Program = parseProgram
+
+    override def next: Program = {
+      val result = _next
+      _next = parseProgram
+      result
     }
+
+    override def hasNext: Boolean = (_next != null)
+
+    protected def parseProgram: Program =
+      if (parser.nextToken == END_ARRAY || parser.isClosed) { null }
+      else objectMapper.readValue(parser, classOf[Program])
+  }
 
   protected def unexpected(token: JsonToken, parser: JsonParser) =
     throw new MalformedResponse(s"Unexpected token ${token}", parser)
@@ -76,15 +83,9 @@ object SearchResult {
     override def notifyStatus(value: Int) { this.status = value }
     override def notifyHit(value: Int) { this.hit = value }
     override def notifyVersion(value: String) { this.version = value }
-    override def notifyPrograms(programs: Stream[Program]) { this.programs = programs.toList }
+    override def notifyPrograms(programs: Iterator[Program]) { this.programs = programs.toList }
     override def getResult: SearchResult = new SearchResult(status, hit, version, programs)
   }
-
-  private def searchResults(parser: JsonParser): Stream[SearchResult] =
-    if (parser.isClosed) Stream.empty
-    else {
-      Stream.empty
-    }
 
   private def intFieldValue(parser: JsonParser): Int = {
     val intValue = parser.nextIntValue(NA)
--- src/test/scala/model/SearchResultSpec.scala
+++ src/test/scala/model/SearchResultSpec.scala
@@ -3,15 +3,59 @@ package com.github.ikuo.garapon4s.model
 import com.github.ikuo.garapon4s._
 
 class SearchResultSpec extends UnitSpec {
-  trait Fixture extends JsonFixture
-
   describe(".parse") {
-    it("should return a SearchResult with programs") {
-      new Fixture {
-        val result = SearchResult.parse(jsonFixtureAsInputStream("search_result/0"))
-        result should not be (null)
-        result shouldBe a [SearchResult]
-        result.programs.head.gtvId should be ("1SJP7E871394280000")
+    describe("without listener") {
+      it("should return a SearchResult with programs") {
+        new JsonFixture {
+          val result = SearchResult.parse(jsonFixtureAsInputStream("search_result/0"))
+          result should not be (null)
+          result shouldBe a [SearchResult]
+          result.status should be (1)
+          result.hit should be (9849)
+          result.version should be ("GTV3.1401190")
+          result.programs.head.gtvId should be ("1SJP7E871394280000")
+        }
+      }
+    }
+
+    describe("with listener") {
+      trait Fixture extends JsonFixture {
+        var iterator: Iterator[Program] = null
+      }
+
+      describe("when program iterator is consumed") {
+        it("should iterates programs without exception") {
+          new Fixture {
+            val listener = new SearchResultListener {
+              override def notifyPrograms(programs: Iterator[Program]) {
+                iterator = programs
+                iterator.toList
+              }
+            }
+
+            SearchResult.parse(jsonFixtureAsInputStream("search_result/0"), listener)
+
+            iterator should not be (null)
+          }
+        }
+      }
+
+      describe("when program iterator is not consumed") {
+        it("should throw an IllegalStateException") {
+          new Fixture {
+            intercept[IllegalStateException] {
+              val listener = new SearchResultListener {
+                override def notifyPrograms(programs: Iterator[Program]) {
+                  iterator = programs
+                }
+              }
+
+              SearchResult.parse(jsonFixtureAsInputStream("search_result/0"), listener)
+
+              iterator should not be (null)
+            }
+          }
+        }
       }
     }
   }
