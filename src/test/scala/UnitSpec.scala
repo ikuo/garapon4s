2f4ad65c9b2ebd9ebfaf4b43e3de20b4ed30c877
Ikuo Matsumura <makiczar@gmail.com>
2014-06-21 17:51:29 +0900

    Cleanup UnitSpec.scala

--- src/test/scala/UnitSpec.scala
+++ src/test/scala/UnitSpec.scala
@@ -2,11 +2,7 @@ package com.github.ikuo.garapon4s
 
 import org.scalatest._
 import org.scalamock.scalatest.MockFactory
-import org.scalamock.scalatest.proxy.{MockFactory => ProxyMockFactory}
-//import org.scalatest.mock._
 
 abstract class UnitSpec extends FunSpec
   with Matchers
   with MockFactory
-  //with ProxyMockFactory
-  //with MockitoSugar
